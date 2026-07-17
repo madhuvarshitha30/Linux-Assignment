130091bead29c15a7861946c52928affd294e0e4
madhuvarshitha30 <madhu.varshitha@prodapt.com>
2026-07-17 17:47:43 +1300

    Add example2.js with variable declarations and greet function

---
+++ Day10/example2.js
@@ -0,0 +1,20 @@
+let name = "John";
+let name = "Alice";
+let isActive = true;
+let salary = null;
+let adress;
+let id = Symbol("id");
+let big = 123456789n;
+
+let student = {
+    name: "John",
+    age: 22
+};
+
+let colors = ["Red", "Blue"];
+
+function greet(){
+    console.log("Hello")
+}
+
+greet()
